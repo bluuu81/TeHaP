efcb658c10e7c65f063b502e7ce6edfe9a00672f
bluuu <bluuu.81@gmail.com>
2023-06-20 23:38:20 +0200

    forbriden files

---
+++ FW/Core/Inc/adc.h
@@ -0,0 +1,15 @@
+/*
+ * adc.h
+ *
+ *  Created on: May 25, 2023
+ *      Author: bluuu
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+void ADC_Print();
+void ADC_DMA_Start();
+float GET_MCU_Temp();
+
+#endif /* INC_ADC_H_ */
---
+++ FW/Core/Inc/bme280.h
@@ -0,0 +1,20 @@
+/*
+ * bme280.h
+ *
+ *  Created on: 18 cze 2023
+ *      Author: bluuu
+ */
+
+#ifndef INC_BME280_H_
+#define INC_BME280_H_
+
+#include "main.h"
+#include "thp_sensors.h"
+
+#define BME280_ADDR				0x76 << 1
+#define BME280_REG_ID			0xD0
+#define BME280_ID_CHK			0x60
+
+uint8_t BME280_check();
+
+#endif /* INC_BME280_H_ */
